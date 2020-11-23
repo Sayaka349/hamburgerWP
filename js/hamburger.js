6dfae6382b452abf81316cc24653f80014456b3b
Sayaka <sykn4q@gmail.com>
2020-11-23 12:04:34 +0900

    initial commit

---
+++ js/hamburger.js
@@ -0,0 +1,5 @@
+$(function(){
+    $('.l-header__menu-icon, .l-sidebar__menu-icon, .c-filter').on('click', function () {
+        $('.l-header__menu-icon, .l-sidebar, .l-sidebar__menu-icon, .c-filter').toggleClass('show');
+    });
+    })
