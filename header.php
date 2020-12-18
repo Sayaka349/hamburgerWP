9285f0af81480f880a8d3f83e9025fa9385e0b02
Sayaka <sykn4q@gmail.com>
2020-12-18 21:46:32 +0900

    お問い合わせフォーム設置と微調整

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="WordPressテーマ作成。RaiseTechの課題でhamburgerサイト構築をしました。">
     <?php wp_head(); ?>
 </head>
 
